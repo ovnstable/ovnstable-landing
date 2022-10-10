78bba37c6772369c363d01e20bc900032bd6a721
Roman <romangolovlyov@gmail.com>
2022-10-10 12:36:28 +0500

    DEV-538 ProductsPage.vue proper view on mobile - done.

--- src/component/pages/use_cases/UseCasesPage.vue
+++ src/component/pages/use_cases/UseCasesPage.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="page-container overflow-hidden">
-        <v-row class="container-row-cases d-flex overflow-hidden justify-center mb-10 ml-15 mr-15">
+        <v-row class="container-row-cases d-flex overflow-hidden justify-center" :class="isMobile ? 'ma-0' : 'ml-15 mr-15 mb-15'">
             <v-col class="text-col" cols="12">
-                <v-row class="title-row mb-10 justify-center main-label" v-observe-visibility="visibilityChanged">
+                <v-row :class="isMobile ? 'mb-0' : 'mb-10'" class="title-row justify-center main-label" v-observe-visibility="visibilityChanged">
                     <label class="accent-text">USD</label><label class="title-text-plus">+</label>
-                    <label class="title-text mb-0">&nbsp;use cases</label>
+                    <label class="title-text">&nbsp;use cases</label>
                 </v-row>
 
-                <v-row class="d-flex justify-space-around card-container-row mb-5" :class="isMobile ? 'flex-column' : 'flex-row'">
+                <v-row class="d-flex justify-space-around card-container-row" :class="isMobile ? 'flex-column' : 'flex-row'">
                     <v-col :cols="isMobile ? 12 : (isTablet ? 6 : 4)">
                         <v-container class="card-container">
-                            <v-row class="mt-12 mb-8 title-row ml-5" justify="start">
+                            <v-row :class="isMobile ? 'ma-5' : 'mt-12 mb-8 ml-5'" class="title-row" justify="start">
                                 <label class="title-label">Cash management</label>
                             </v-row>
 
-                            <v-row class="mb-12 desc-row" justify="start">
+                            <v-row :class="isMobile ? 'mb-5' : 'mb-12'" class="desc-row" justify="start">
                                 <label class="desc-label mx-8">Do nothing and earn yield in stables on your stables right in the wallet via daily rebase</label>
                             </v-row>
                         </v-container>
@@ -22,11 +22,11 @@
 
                     <v-col :cols="isMobile ? 12 : (isTablet ? 6 : 4)">
                         <v-container class="card-container">
-                            <v-row class="mt-12 mb-8 title-row ml-5" justify="left">
+                            <v-row :class="isMobile ? 'ma-5' : 'mt-12 mb-8 ml-5'" class="title-row" justify="left">
                                 <label class="title-label">Liquidity Pools in dexes</label>
                             </v-row>
 
-                            <v-row class="mb-12 desc-row" justify="start">
+                            <v-row :class="isMobile ? 'mb-5' : 'mb-12'" class="desc-row" justify="start">
                                 <label class="desc-label mx-8">Get fees, rewards and yield on the USD+ pairs in pools compatibe with Uni V2, Curve and Balancer</label>
                             </v-row>
                         </v-container>
@@ -34,40 +34,41 @@
 
                     <v-col :cols="isMobile ? 12 : (isTablet ? 6 : 4)">
                         <v-container class="card-container">
-                            <v-row class="mt-12 mb-8 title-row ml-5" justify="left">
+                            <v-row :class="isMobile ? 'ma-5' : 'mt-12 mb-8 ml-5'" class="title-row" justify="left">
                                 <label class="title-label">Treasury liquidity in pools</label>
                             </v-row>
 
-                            <v-row class="mb-12 desc-row" justify="start">
-                                <ul class="desc-label mx-8">
+                            <v-row :class="isMobile ? 'mb-5' : 'mb-12'" class="desc-row" justify="start">
+                                <ul class="desc-label mx-6">
                                     <li class="list-desc">Earn yield on stablecoins you provide to DEXes paired against your project token;</li>
                                     <li class="list-desc">Create appreciation pressure on the project's token;</li>
                                 </ul>
-                                <label class="link mx-8 mt-6" @click="openModal(showHall)">How it works</label>
+                                <label :class="isMobile ? 'mx-8 mt-4' : 'mx-8 mt-6'" class="link" @click="openModal(showHall)">How it works</label>
                             </v-row>
                         </v-container>
                     </v-col>
 
                     <v-col :cols="isMobile ? 12 : (isTablet ? 6 : 6)">
                         <v-container class="card-container second-row">
-                            <v-row class="mt-12 mb-8 title-row ml-5" justify="space-between">
+                            <v-row :class="isMobile ? 'ma-5' : 'mt-12 mb-8 ml-5'" class="title-row" justify="space-between">
                                 <label class="title-label">Yield on collateral</label>
-                                <label class="in-development mr-10 mb-5" >In development</label>
+                                <label :class="isMobile ? 'ml-auto' : 'mr-10 mb-5'" class="in-development" >In development</label>
                             </v-row>
 
-                            <v-row class="mb-12 desc-row" justify="start">
+                            <v-row :class="isMobile ? 'mb-5' : 'mb-12'" class="desc-row" justify="start">
                                 <label class="desc-label mx-8">Pledge USD+ as collateral and earn yield, potentially, exceeding the borrowing rate</label>
                             </v-row>
                         </v-container>
                     </v-col>
                     <v-col :cols="isMobile ? 12 : (isTablet ? 6 : 6)">
                         <v-container class="card-container second-row last-desc">
-                            <v-row class="mt-12 mb-8 title-row ml-5" justify="space-between">
+                            <v-row :class="isMobile ? 'ma-5' : 'mt-12 mb-8 ml-5'" class="title-row" justify="space-between">
                                 <label class="title-label">Yield on CEX liquidity</label>
-                                <label class="in-development mr-10 mb-5">In development</label>
+                                <v-spacer></v-spacer>
+                                <label :class="isMobile ? 'ml-auto' : 'mr-10 mb-5'" class="in-development">In development</label>
                             </v-row>
 
-                            <v-row class="mb-16 desc-row" justify="start">
+                            <v-row :class="isMobile ? 'mb-5' : 'mb-16'" class="desc-row" justify="start">
                                 <label class="desc-label mx-8">Earn DeFi yield on stablecoin liquidity kept in USD+</label>
                             </v-row>
                         </v-container>
@@ -279,8 +280,7 @@ export default {
     }
 
     .container-row-cases {
-        margin-left: 0 !important;
-        margin-right: 0 !important;
+        margin: 0 !important;
     }
 
     .main-label {
@@ -446,6 +446,10 @@ export default {
 .title-row {
     z-index: 100;
     position: relative;
+}
+
+.title-label {
+    font-family: 'Roboto', sans-serif;
 }
 
 .title-row-unbordered {
